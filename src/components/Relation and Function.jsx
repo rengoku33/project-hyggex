7e86b40918cf9b7ea3119a7049b05a08ba562813
Kiran Vignesh <rajkiranvignesh369@gmail.com>
2024-04-22 21:54:50 +0530

    finished bottom components, remaining: accordion and style alteration

--- src/components/Relation and Function.jsx
+++ src/components/Relation and Function.jsx
@@ -1,22 +1,41 @@
 import React, { useState } from "react";
-import { FiArrowLeft, FiArrowRight, FiRefreshCw, FiMaximize2 } from "react-icons/fi";
+import logoBOT from '../assets/logo-bot.png';
+import { FaAngleLeft, FaAngleRight, FaPlusCircle  } from "react-icons/fa";
+import { BiFullscreen } from "react-icons/bi";
+
 import { motion } from 'framer-motion';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faSync } from '@fortawesome/free-solid-svg-icons';
 
 const Relation = () => {
-    const [selected, setSelected] = useState(0);
+    const [selected, setSelected] = useState(1);
     const links = ["Study", "Quiz", "Test", "Game", "Others"];
 
     const [isFlipped, setIsFlipped] = useState(false);
     const [isAnimating, setIsAnimating] = useState(false);
 
+    const [count, setCount] = useState(1);
+
     const handleClick = () => {
 
-        if(!isAnimating){
+        if (!isAnimating) {
             setIsAnimating(true);
             setIsFlipped(!isFlipped);
         }
-        
+
     };
+
+    const increment = () => {
+        if (count < 10) {
+            setCount(count + 1);
+        }
+    }
+    const decrement = () => {
+        if (count > 1) {
+            setCount(count - 1);
+        }
+    }
+
     return (
         <div>
             {/* title */}
@@ -26,6 +45,7 @@ const Relation = () => {
 
             {/* 3x container - subnav + flip box + box footer */}
             <div className="relative top-7 w-full h-full flex flex-col items-center justify-center">
+
                 {/* subnav */}
                 <div className="bg-transparent h-16 flex justify-center items-center">
                     {links.map((link, index) => (
@@ -41,31 +61,42 @@ const Relation = () => {
                 {/* flip box */}
                 <div onClick={handleClick} className="flip-card rounded-2xl relative w-2/4 h-60 bg-transparent flex items-center justify-center">
                     <motion.div
-                    className='flip-card-inner w-[100%] h-[100%]'
-                    initial={false}
-                    animate={{rotateY: isFlipped ? 180 : 360}}
-                    transition={{duration: 0.6, animationDirection: "normal"}}
-                    onAnimationComplete={() => setIsAnimating(false)}
+                        className='flip-card-inner w-[100%] h-[100%]'
+                        initial={false}
+                        animate={{ rotateY: isFlipped ? 180 : 360 }}
+                        transition={{ duration: 0.6, animationDirection: "normal" }}
+                        onAnimationComplete={() => setIsAnimating(false)}
                     >
-                    {/* <div className="absolute flex items-center justify-center w-full h-full">
+                        {/* <div className="absolute flex items-center justify-center w-full h-full">
                         <span className="text-white text-2xl font-semibold">{isFlipped ? '9 + 6 + 7x - 2x - 3' : '5x + 12'}</span>
                     </div> */}
-                    
-                    <div className="flip-card-front w-[100%] h-[100%] bg-cover-border-[1px] flex items-center justify-center flip-card rounded-2xl bg-gradient-to-l from-blue-900 to-blue-500 ">
-                        <h1 className="text-white text-2xl font-semibold">9 + 6 + 7x - 2x - 3</h1>
-                    </div>
-                    <div className="flip-card-back w-[100%] h-[100%] bg-cover-border-[1px] flex items-center justify-center flip-card rounded-2xl bg-gradient-to-l from-blue-900 to-blue-500 ">
-                        <h1 className="text-white text-2xl font-semibold">5x + 12</h1>
-                    </div>
+
+                        <div className="flip-card-front w-[100%] h-[100%] bg-cover-border-[1px] flex items-center justify-center flip-card rounded-2xl bg-gradient-to-tr from-sky-500 to-blue-950">
+                            <h1 className="text-white text-2xl font-semibold">9 + 6 + 7x - 2x - 3</h1>
+                        </div>
+                        <div className="flip-card-back w-[100%] h-[100%] bg-cover-border-[1px] flex items-center justify-center flip-card rounded-2xl bg-gradient-to-tr from-sky-500 to-blue-950">
+                            <h1 className="text-white text-2xl font-semibold">5x + 12</h1>
+                        </div>
                     </motion.div>
                 </div>
-                {/* <div className="absolute bottom-0 left-0 right-0 z-10 bg-white h-16 flex justify-center items-center">
-                    <FiArrowLeft className="text-xl text-gray-500 absolute left-4 cursor-pointer" />
-                    <FiArrowRight className="text-xl text-gray-500 absolute right-4 cursor-pointer" />
-                    <FiRefreshCw className="text-xl text-gray-500 absolute left-1/2 transform -translate-x-1/2 cursor-pointer" />
-                    <FiMaximize2 className="text-xl text-gray-500 absolute right-1/2 transform translate-x-1/2 cursor-pointer" />
-                </div> */}
+                {/* bot-box */}
+                <div className="bg-transparent h-16 flex justify-center items-center">
+                    <span className=" mx-24 text-blue-700 cursor-pointer" style={{ fontSize: 27, fontWeight: 600 }}>&#10227;</span>
+                    <span onClick={decrement} className=" mx-6 bg-gradient-to-t from-blue-700 to-blue-950 text-white rounded-full px-1 py-1 cursor-pointer" style={{ fontSize: 25, fontWeight: 600 }}><FaAngleLeft className="text-white cursor-pointer" /></span>
+                    <span className=" mx-6 cursor-pointer font-bold">{count}/10</span>
+                    <span onClick={increment} className=" mx-6 bg-gradient-to-t from-blue-700 to-blue-950 text-white rounded-full px-1 py-1 cursor-pointer" style={{ fontSize: 25, fontWeight: 600 }}><FaAngleRight className="text-white cursor-pointer" /></span>
+                    <span className=" mx-24 text-blue-700 cursor-pointer" style={{ fontSize: 25, fontWeight: 600 }}><BiFullscreen className="cursor-pointer" /></span>
+                </div>
             </div>
+                {/* bot img */}
+                <div className="relative top-7 w-full h-full flex justify-center items-center">
+                <img src={logoBOT} alt='High-jacks' className="h-24 w-60 mr-[50%]"/>
+                <div className=" flex mr-25">
+                <FaPlusCircle className=" text-blue-700" />
+                <h1>Create Flashcard</h1>
+                </div>
+                </div>
+
         </div>
     )
 }
